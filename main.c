63af46b72ec4060a1b7356a056b814e4c7fefb9b
diasbouk <boukdsaid@gmail.com>
2024-05-10 18:24:57 +0100

    CLEANED CODE AND REMOVE SOME USELESS STUFF ==!

--- main.c
+++ main.c
@@ -9,7 +9,16 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+
+/* Defining a delimiter to use in strtok  */
 #define DELIM "\t \n"
+
+
+/*
+ * _strlen - Get the length of a string
+ * @str: string to get length
+ * Return: number of chars in str (length)
+ */
 
 int _strlen(char *str)
 {
@@ -18,6 +27,17 @@ int _strlen(char *str)
 		i++;
 	return (i);
 }
+
+
+
+/*
+ * _strdup - Duplicates a string (allocates memory)
+ * @str: string to duplicate
+ * Return: new string duplicated (allocated)
+ * Or NULL if it fails to do so
+ */
+
+
 
 char *_strdup(char *str)
 {
@@ -30,6 +50,13 @@ char *_strdup(char *str)
 	token[i] = '\0';
 	return (token);
 };
+
+
+/*
+ * _free - Frees a 2D arrays of strings
+ * @cmd: array to free
+ * Return: None return ,  void func
+ */
 
 void _free(char **cmd)
 {
@@ -46,6 +73,18 @@ void _free(char **cmd)
 	cmd = NULL;
 }
 
+
+
+/*
+ * _getline - self  made of the getline func
+ * reads a line from stream 
+ * @buffer: Buffer to hold input
+ * @nbuf: number of chars to be read
+ * @stream: file-descriptor to read from (mostly STDIN)
+ * Return: Number of chars readed 
+ * or 0 if it reached EOF (end of line)
+ */
+
 size_t _getline(char **buffer, size_t *nbuf, FILE *stream)
 {
 	*buffer = malloc(sizeof(char) * 1000);
@@ -53,6 +92,14 @@ size_t _getline(char **buffer, size_t *nbuf, FILE *stream)
 
 	return (read(1, *buffer, 1000));
 }
+
+
+/*
+ * split - splits a long line (command) into arrays of strings
+ * Command arguments
+ * @str: full command to split
+ * Return: 2D array of strnigs or NULL if it failed
+ */
 
 char **split(char *str)
 {
@@ -93,20 +140,31 @@ char **split(char *str)
 	return (full_cmd);
 }
 
+
+/*
+ * main - main function or / 
+ * Entry point of the programm
+ * @argc: number of args passed at execution 
+ * @argv: 2D array of args
+ * @envp: environnement vars in form of 2D arrays of strings
+ * Return: 0 on success else an int
+ */
+
 int main(int ac, char **av, char **envp)
 {
-	char *buffer = NULL, *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 	;
 	size_t MAX_BUF = 0;
 	int i = 0, status;
-	char **cmd = NULL;
+	char **cmd = NULL, *buffer;
 	size_t nchars;
 	pid_t forked;
 	while (1)
 	{
+		/* checking if its in interactive mode or not */
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "[dias@penguin]# ", 16);
 
+		/* reading from STDIN_FILENO*/
 		nchars = getline(&buffer, &MAX_BUF, stdin);
 		if (nchars == -1)
 		{
@@ -114,22 +172,28 @@ int main(int ac, char **av, char **envp)
 			exit(0);
 		}
 
+		/* split read command */
 		cmd = split(buffer);
 		if (!cmd || nchars == 1)
 			continue;
+		/* forking the next codeblock */
 		forked = fork();
 		if (forked == 0)
 		{
+			/* executing the command after split */
 			if (execve(cmd[0], cmd, envp) == -1)
 				_free(cmd);
 		}
 
 		else
+		/* if its not the child process 
+		 * it should wait to child to finish
+		 */
 			waitpid(forked, &status, 0);
 
 		if (cmd != NULL)
 			_free(cmd);
 	}
 	free(buffer);
-	return (0);
+	return (EXIT_SUCCESS);
 }
